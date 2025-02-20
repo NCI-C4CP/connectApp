0385f698636036c48ae19bc0386294b06a426ce1
Joe Armani <93854858+JoeArmani@users.noreply.github.com>
2025-02-20 16:16:19 -0500

    update AppVersion

--- serviceWorker.js
+++ serviceWorker.js
@@ -2,7 +2,7 @@ importScripts(
 	'https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js'
 );
 
-const appVersion = 'v25.1.2';
+const appVersion = 'v25.2.1';
 workbox.setConfig({ debug: false });
 const { registerRoute } = workbox.routing;
 const { CacheFirst, NetworkFirst, StaleWhileRevalidate, NetworkOnly } =
