bbf9807eef9e4664f0889c668c8b0b2d76c4a0d1
anthonypetersen <tonypetersen@hotmail.com>
2026-02-03 12:45:37 -0600

    logic

--- js/shared.js
+++ js/shared.js
@@ -3879,9 +3879,10 @@ export const setModuleAttributes = async (data, modules, collections) => {
         }
     }
 
-    // The 2026 Return of Results Preference Survey is available for participants verified on or before August 1, 2026
-    if (data[fieldMapping.ROIPreference2026.statusFlag]) {
+    // Survey is closed: only show for participants who have already submitted it
+    if (data[fieldMapping.ROIPreference2026.statusFlag] === fieldMapping.moduleStatus.submitted) {
         modules['2026 Return of Results Preference Survey'].enabled = true;
+        modules['2026 Return of Results Preference Survey'].completed = true;
     }
 
     if (data[fieldMapping.ROIPreference2026.statusFlag] === fieldMapping.moduleStatus.submitted) {
