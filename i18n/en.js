b1eb8abd7232c90f5f9c9826ffcb928af175be32
amber-emmes <ablaylock@emmes.com>
2026-05-11 15:02:56 -0400

    1622 Updates
    For [1622](https://github.com/episphere/connect/issues/1622). The option to continue with an address not found by the address validation API is now available as a more prominent button rather than an easily missed link in the footer.

--- i18n/en.js
+++ i18n/en.js
@@ -1337,7 +1337,8 @@ const en = {
         "old": "Old",
         "noNotifications": "No notifications found!",
         "invalidAddressDescription": "The address you entered may not be valid. Please check your entry below. If the address is not correct, please click ‘Go Back’ and correct the address. If the address is correct, please click ‘Continue with Address’. We are only able to send Connect communications and packages to valid addresses.",
-        "invalidAddressFooter": "If you are having problems fixing these errors and can’t submit your profile, please reach out to the <a href=\"https://myconnect.cancer.gov/support\" target=\"_blank\">Connect Support Center</a>  for help or <a id=\"continueBtn\" style=\"cursor: pointer;text-decoration: underline\">Continue with Address</a> as shown."
+        "continueWithAddress": "Continue with Address",
+        "invalidAddressFooter": "If you are having problems fixing these errors and can’t submit your profile, please reach out to the <a href=\"https://myconnect.cancer.gov/support\" target=\"_blank\">Connect Support Center</a>  for help.",
     },
     "settingsHelpers": {
         "suffixJr": "Jr.",
